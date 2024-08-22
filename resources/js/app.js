6e6f499a7761546fd6c2257c708b7903ac27dd92
Janosch Püschel <janosch.pueschel@gmail.com>
2024-08-22 20:29:24 +0200

    feat: add default Page layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "../css/app.css";
 import "primeicons/primeicons.css";
+import Layout from "./Shared/Layout.vue";
 
 import { createApp, h } from "vue";
 import { createInertiaApp, Link } from "@inertiajs/vue3";
@@ -7,7 +8,9 @@ import { createInertiaApp, Link } from "@inertiajs/vue3";
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
-        return pages[`./Pages/${name}.vue`];
+        let page = pages[`./Pages/${name}.vue`];
+        page.default.layout = page.default.layout || Layout;
+        return page;
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
