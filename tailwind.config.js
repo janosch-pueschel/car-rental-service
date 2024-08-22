8e8b713369824486416753dde03b3090ab5864bf
Janosch Püschel <janosch.pueschel@gmail.com>
2024-08-22 19:03:55 +0200

    feat(design): add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ export default {
                 poppins: ["Poppins", "sans-serif"],
                 "work-sans": ["Work Sans", "sans-serif"],
             },
+            colors: {
+                blue: "#003366",
+                gold: "#f5bf03",
+                "light-grey": "#E0E0E0",
+                "dark-grey": "#4D4D4D",
+            },
         },
     },
     plugins: [],
