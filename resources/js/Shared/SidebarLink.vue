45bd0658fa22155755d7c81e2008d63e4708a224
Janosch Püschel <janosch.pueschel@gmail.com>
2024-08-22 20:08:21 +0200

    feat: add SidebarLink component

---
+++ resources/js/Shared/SidebarLink.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps({ active: Boolean });
+</script>
+
+<template>
+    <Link
+        class="px-3 py-2 hover:font-semibold"
+        :class="{ 'bg-gold rounded-md': active }"
+        ><slot></slot
+    ></Link>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import "../css/app.css";
 import "primeicons/primeicons.css";
 
 import { createApp, h } from "vue";
-import { createInertiaApp } from "@inertiajs/vue3";
+import { createInertiaApp, Link } from "@inertiajs/vue3";
 
 createInertiaApp({
     resolve: (name) => {
@@ -12,6 +12,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .component("Link", Link)
             .mount(el);
     },
 });
