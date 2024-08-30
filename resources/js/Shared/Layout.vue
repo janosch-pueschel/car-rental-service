263ef3eb1a48411020a787fcc2df63a66f97b4b7
Janosch Püschel <janosch.pueschel@gmail.com>
2024-08-30 23:39:55 +0200

    refactor: update design

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar.vue";
 </script>
 
 <template>
-    <div class="flex space-x-5">
+    <div class="flex space-x-5 bg-white">
         <Sidebar />
         <div>
             <slot></slot>
