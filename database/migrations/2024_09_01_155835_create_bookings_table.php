65cf017f6bbab645f55b48c82e96c9992c2c1872
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-02 22:34:47 +0200

    chore(database): add migrations

---
+++ database/migrations/2024_09_01_155835_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Driver;
+use App\Models\FuelType;
+use App\Models\Transmission;
+use App\Models\VehicleCategory;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('departure');
+            $table->dateTime('return');
+            $table->foreignIdFor(Driver::class)->constrained();
+            $table->foreignIdFor(VehicleCategory::class)->constrained();
+            $table->foreignIdFor(FuelType::class)->constrained();
+            $table->foreignIdFor(Transmission::class)->constrained();
+            $table->smallInteger('price_per_day');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
