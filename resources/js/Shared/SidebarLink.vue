3487e65a413237728e730006b3a99ab26be3a15e
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-06 18:41:19 +0200

    refactor(design): change active link styling

--- resources/js/Shared/SidebarLink.vue
+++ resources/js/Shared/SidebarLink.vue
@@ -6,7 +6,7 @@ defineProps<{ active: boolean; icon?: string }>();
     <Link
         class="px-5 py-2 flex items-center"
         :class="{
-            'bg-green rounded-full hover:font-normal': active,
+            'bg-white/25 border-l-4 px-4 hover:font-normal': active,
             'hover:font-semibold': !active,
         }"
         ><i v-if="icon" class="pi mr-2 text-sm" :class="icon"></i><slot></slot
