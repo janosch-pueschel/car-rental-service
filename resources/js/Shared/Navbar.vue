c54cbd7c0156f5e4a9d269ea650c08ad3129a3cd
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-06 18:45:02 +0200

    feat(component): add Navbar component

---
+++ resources/js/Shared/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-black text-white flex py-3">
+        <div class="w-64 flex justify-center items-center">
+            <img
+                src="../../img/europcar_logo.png"
+                alt="europcar_logo"
+                class="w-40"
+            />
+        </div>
+    </div>
+</template>
