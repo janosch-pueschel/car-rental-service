548c486ba9cb8d044b3af4dffe2d81904ac0856a
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-08 13:55:02 +0200

    feat: add Button component

---
+++ resources/js/Shared/Button.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ title: string; type?: string; icon?: string }>();
+</script>
+
+<template>
+    <component
+        :is="type ? 'Link' : 'button'"
+        class="border border-green py-2 px-3 rounded w-fit h-fit text-green font-medium hover:bg-green hover:text-white transition-colors"
+    >
+        <i v-if="icon" class="pi mr-2 text-sm" :class="icon"></i>{{ title }}
+    </component>
+</template>
