5ed470a3e2c401943ffc05e23487639889064e47
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-10 20:35:39 +0200

    refactor(database): remove requirement of unique email

--- database/migrations/2024_09_01_154449_create_drivers_table.php
+++ database/migrations/2024_09_01_154449_create_drivers_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('gender');
             $table->date('birthdate');
-            $table->string('email')->unique();
+            $table->string('email');
         });
     }
 
