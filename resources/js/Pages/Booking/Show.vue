1fbc8e00fc810f62e7033576dd8e3d9f4ca8434a
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-11 18:29:32 +0200

    feat: add page to view booking

---
+++ resources/js/Pages/Booking/Show.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Button from "../../Shared/Button.vue";
+import Heading from "../../Shared/Heading.vue";
+
+defineProps({
+    booking: Object,
+    driver: Object,
+    vehicleCategory: Object,
+    fuelType: Object,
+    transmission: Object,
+});
+</script>
+
+<template>
+    <Heading title="Booking Details"></Heading>
+
+    <div class="border py-5 rounded-lg">
+        <p>Name: {{ driver?.first_name }} {{ driver?.last_name }}</p>
+        <p>Departure: {{ booking?.departure }}</p>
+        <p>Return: {{ booking?.return }}</p>
+        <p>
+            Vehicle Details: {{ vehicleCategory?.name }}, {{ fuelType?.name }},
+            {{ transmission?.name }}
+        </p>
+        <p>Price per Day: {{ booking?.price_per_day }}</p>
+    </div>
+</template>
