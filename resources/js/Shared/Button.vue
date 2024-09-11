9cc6a51334326a0ae77f58449173804c354d3003
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-11 17:50:55 +0200

    feat(design): add props to allow conditional button styles

--- resources/js/Shared/Button.vue
+++ resources/js/Shared/Button.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
-defineProps<{ title: string; type?: string; icon?: string }>();
+defineProps<{
+    title: string;
+    link?: boolean;
+    linkType?: string;
+    icon?: string;
+    disabled?: boolean;
+    styleType?: string;
+}>();
 </script>
 
 <template>
     <component
-        :is="type ? 'Link' : 'button'"
-        class="border border-green py-2 px-3 rounded w-fit h-fit text-green font-medium hover:bg-green hover:text-white transition-colors"
+        :is="link ? linkType : 'button'"
+        class="py-2 px-3 rounded w-fit h-fit font-medium transition-colors border"
+        :class="{
+            ' border-green text-green hover:bg-green hover:text-white':
+                !disabled,
+            ' border-dark-grey text-dark-grey': disabled,
+            'border-red-500 text-red-500 hover:bg-red-500 hover:text-white':
+                styleType === 'danger',
+        }"
     >
         <i v-if="icon" class="pi mr-2 text-sm" :class="icon"></i>{{ title }}
     </component>
