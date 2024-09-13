b2c353bac65cfe6fa0e7b1fd8df8a4beed5372b6
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-13 21:38:49 +0200

    fix: update component to accept v-model data binding

--- resources/js/Shared/Form/FormInput.vue
+++ resources/js/Shared/Form/FormInput.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+    label: string;
+    id: string;
+}>();
+
+const model = defineModel();
+
+const updateModel = (inputValue: string) => {
+    model.value = inputValue;
+};
+</script>
+
 <template>
-    <input class="border rounded-full px-5 py-2 text-dark-grey" />
+    <label :for="id" class="text-light-grey">{{ label }}</label>
+    <input
+        class="border rounded-full px-5 py-2 text-dark-grey"
+        :value="modelValue"
+        @input="updateModel($event.target.value)"
+        v-bind="$attrs"
+    />
 </template>
