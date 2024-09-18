4b4b4749a6153752136bd9ad399d0526837adb42
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-18 19:25:01 +0200

    feat: add function to seed database with admin user for testing purpose

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\UserRole;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,18 +14,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('password');
-            $table->rememberToken();
+            $table->foreignIdFor(UserRole::class)->constrained();
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
@@ -43,7 +38,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        /* Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions'); */
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Booking;
+use App\Models\Driver;
+use App\Models\FuelType;
+use App\Models\Transmission;
 use App\Models\User;
+use App\Models\UserRole;
+use App\Models\VehicleCategory;
+use Illuminate\Container\Attributes\DB;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB as FacadesDB;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +22,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (!UserRole::exists()) {
+            $userRoles = [
+                ['name' => 'admin'], ['name' => 'employee']
+            ];
+    
+            FacadesDB::table('user_roles')->insert($userRoles);
+
+            User::create([
+                'username' => 'admin',
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'password' => bcrypt('password'),
+                'user_role_id' => 1]);
+        }
+
+        Driver::factory(40)->create();
+        VehicleCategory::factory(7)->create();
+        FuelType::factory(3)->create();
+        Transmission::factory(2)->create();
+        Booking::factory()->count(50)->create();
     }
 }
