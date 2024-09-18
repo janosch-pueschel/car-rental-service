d3e1a088a00fef013dc239f227be365841dc2e6c
Janosch Püschel <janosch.pueschel@gmail.com>
2024-09-18 19:20:55 +0200

    refactor(design): update form input layout

--- resources/js/Shared/Form/FormInput.vue
+++ resources/js/Shared/Form/FormInput.vue
@@ -12,11 +12,13 @@ const updateModel = (inputValue: string) => {
 </script>
 
 <template>
-    <label :for="id" class="text-light-grey">{{ label }}</label>
-    <input
-        class="border rounded-full px-5 py-2 text-dark-grey"
-        :value="modelValue"
-        @input="updateModel($event.target.value)"
-        v-bind="$attrs"
-    />
+    <div class="flex flex-col">
+        <label :for="id" class="text-light-grey">{{ label }}</label>
+        <input
+            class="border rounded-full px-5 py-2 text-dark-grey"
+            :value="modelValue"
+            @input="updateModel($event.target.value)"
+            v-bind="$attrs"
+        />
+    </div>
 </template>
